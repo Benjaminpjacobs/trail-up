03771860e387308c4d6e8d0641f57f6d6e5f6878
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-07-26 08:59:51 -0600

    filters search params in search controller

--- app/controllers/events/search_controller.rb
+++ app/controllers/events/search_controller.rb
@@ -2,7 +2,7 @@ class Events::SearchController < ApplicationController
   include Location
 
   def index
-    @events = Event.send(params[:search_by].to_sym, search_params)
+    @events = Event.send(filter_params.to_sym, search_params) if filter_params
   end
 
   private
@@ -14,5 +14,11 @@ class Events::SearchController < ApplicationController
         query: params[:event_search],
         end_date: (params[:end_date].nil? || params[:end_date].empty?) ? params[:event_search] : params[:end_date],
       }
+    end
+    
+    def filter_params
+      if params[:search_by].in?(%w(by_name by_date by_location by_trail))
+        params[:search_by]
+      end
     end
 end
